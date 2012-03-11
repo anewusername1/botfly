2f54bb0d13f4941a5daae74052cc56308ef9b6fe
Tracey Eubanks <tracey.eubanks1337@gmail.com>
2012-03-10 21:52:51 -0700

    starting irc bot

---
+++ bot.h
@@ -0,0 +1,9 @@
+/*
+ *  bot.h
+ *  testing
+ *
+ *  Created by MoneyDesktop on 3/10/12.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+	for (int i = 0; i < 10; i++) {
+		printf("your mom");
+	}
+    return 0;
+}
